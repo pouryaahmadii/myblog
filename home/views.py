151c596633ad396203ff492b31d0b14654f14e7d
Pourya Ahmadi <pouryaaahmadii@gmail.com>
2025-12-25 17:23:12 +0330

    modified home view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def home(request):
-    return render(request, 'home.html')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
